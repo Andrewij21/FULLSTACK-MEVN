cb88227de62e8b8a6bbaa4c1ba40502b526f4554
Andrewij21 <andrewijaya988@gmail.com>
2023-01-18 15:39:39 +0700

    Add interceptors for authentication

---
+++ frontend/src/api.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import store from "@/store/index.js";
+import authServices from "./services/authServices";
+import router from "./router";
+
+const instence = axios.create({
+  withCredentials: true,
+  baseURL: "http://localhost:5000/api/v1/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instence.interceptors.request.use(
+  async function (config) {
+    const token = store.state.auth.token;
+    // if(!token && config.url == "user/refresh"){
+
+    // }else if(!token){
+    //   const refresh = await authServices.refreshToken();
+    //   console.log({ refresh });
+    //   return config;
+    // }
+    // if (!token) {
+    //   console.info("GADA TOKENNNN");
+
+    // }
+    config.headers.Authorization = `Bearer ${token || ""}`;
+    console.log({ req: "request", config });
+    return config;
+  },
+  function (error) {
+    return error;
+  }
+);
+instence.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    const originalRequest = error.config;
+
+    // CEK REFRESH TOKEN STATUS
+    if (
+      (error.response.status == 403 || error.response.status == 401) &&
+      originalRequest.url === "user/refresh"
+    ) {
+      console.log("ExpiredToken");
+      localStorage.removeItem("isAuth");
+      router.push({ path: "/login" });
+      return error;
+    }
+
+    // CEK ACCESS TOKEN
+    if (error.response.status == 401 || error.response.status == 403) {
+      return authServices.refreshToken().then((val) => {
+        console.log({ refresh: val });
+        if (val.status == 200) {
+          store.dispatch("SET_TOKEN", val.data.accessToken);
+          originalRequest.headers = {
+            Authorization: `Bearer ${val.data.accessToken}`,
+          };
+          return axios.request(originalRequest);
+        }
+      });
+    }
+    return error;
+  }
+);
+
+export default instence;
